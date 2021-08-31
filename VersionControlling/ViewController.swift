e9120b75560d66f90cd9aedecef8226cae83f1f9
LinuxPlus <linuxplus@LinuxPluss-Mac.local>
2021-08-31 00:03:28 -0700

    vc

--- VersionControlling/ViewController.swift
+++ VersionControlling/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         print("ds")
+        print("dgfd")
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
